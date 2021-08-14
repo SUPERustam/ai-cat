427cccac142b6c9386cd80917757d9f0763b1870
SUPERustam <498rustam@gmail.com>
2021-08-14 12:06:42 +0300

    🙂 Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,4 +1,6 @@
-python -m venv venv
-source venv/bin/activate
+git config --global http.postBuffer 1048576000
+git config --global http.version HTTP/1.1
+python -m venv .env
+source .env/bin/activate
 pip install --upgrade pip setuptools mypy pylint autoflake isort autopep8
 pip install -r requirements.txt
